d31b1a6cc7f51f0656767f6db0109d9e65b7b8b0
Rajan Kathiriya <rajankathiriya99@gmail.com>
2023-03-30 17:08:31 +0530

    gh

--- material-kit-react-main/src/App.js
+++ material-kit-react-main/src/App.js
@@ -9,7 +9,7 @@ import { StyledChart } from './components/chart';
 import ScrollToTop from './components/scroll-to-top';
 
 
-// ----------------------------------------------------------------------
+
 
 export default function App() {
   return (
